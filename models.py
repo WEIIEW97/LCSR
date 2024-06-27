b6627454be1c0f0d6c8f8d7b56253aefa2565827
williamwei <williamwei9707@gmail.com>
2024-06-27 17:03:24 +0800

    working on dataterm class.

---
+++ models.py
@@ -0,0 +1,66 @@
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+
+import numpy as np
+from .regularizer import TDV
+from typing import Optional, List, Dict
+
+class Dataterm(nn.Module):
+    """
+    Basic dataterm function
+    """
+
+    def __init__(self, config):
+        super(Dataterm, self).__init__()
+
+    def forward(self, x, *args):
+        raise NotImplementedError
+
+    def energy(self):
+        raise NotImplementedError
+
+    def prox(self, x, *args):
+        raise NotImplementedError
+
+    def grad(self, x, *args):
+        raise NotImplementedError
+    
+class L2Dataterm(Dataterm):
+    def __init__(self, config):
+        super(L2Dataterm, self).__init__(config)
+
+    def energy(self, u, u0, lmda):
+        return 0.5*lmda*(u-u0)**2
+
+    def prox(self, u, u0, tau, lmda):
+        return (u + tau*lmda*u0) / (1 + tau*lmda)                                                                                                                                                                                                                                                                                                                                                                                     
+
+    def grad(self, u, u0, lmda):
+        return lmda*(u-u0)
+
+class WeightedL1Dataterm(Dataterm):
+    def __init__(self, config):
+        super(WeightedL1Dataterm, self).__init__(config)
+
+    def energy(self, u, u0, gamma, w):
+        return gamma*torch.sum(w * torch.abs(u - u0))
+
+    def prox(self, u, u0, gamma, tau, w):
+        return u0+torch.max(0, torch.abs(u-u0)-tau*gamma*w)*torch.sign(u-u0)
+
+    def grad(self, u, u0, gamma, w):
+        return gamma*w*torch.sign(u-u0)    
+    
+class L1Dataterm(Dataterm):
+    def __init__(self, config):
+        super(L1Dataterm, self).__init__(config)
+
+    def energy(self, u, u0, gamma):
+        return gamma*torch.sum(torch.abs(u - u0))
+
+    def prox(self, u, u0, gamma, tau):
+        return u0+torch.max(0, torch.abs(u-u0)-tau*gamma)*torch.sign(u-u0)
+
+    def grad(self, u, u0, gamma):
+        return gamma*torch.sign(u-u0) 
