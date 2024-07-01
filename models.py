4a0e58bd1a250cdfe3305d0898938ded17ee6964
william <williamwei9707@gmail.com>
2024-07-01 09:29:16 +0800

    working and thinking on the training procedure. Need more details.

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ class Dataterm(nn.Module):
     Basic dataterm function
     """
 
-    def __init__(self, config):
+    def __init__(self):
         super(Dataterm, self).__init__()
 
     def forward(self, x, *args):
@@ -27,8 +27,8 @@ class Dataterm(nn.Module):
         raise NotImplementedError
     
 class L2Dataterm(Dataterm):
-    def __init__(self, config):
-        super(L2Dataterm, self).__init__(config)
+    def __init__(self):
+        super(L2Dataterm, self).__init__()
 
     def energy(self, u, u0, lmda):
         return 0.5*lmda*(u-u0)**2
@@ -40,8 +40,8 @@ class L2Dataterm(Dataterm):
         return lmda*(u-u0)
 
 class WeightedL1Dataterm(Dataterm):
-    def __init__(self, config):
-        super(WeightedL1Dataterm, self).__init__(config)
+    def __init__(self):
+        super(WeightedL1Dataterm, self).__init__()
 
     def energy(self, u, u0, gamma, w):
         return gamma*torch.sum(w * torch.abs(u - u0))
@@ -53,14 +53,34 @@ class WeightedL1Dataterm(Dataterm):
         return gamma*w*torch.sign(u-u0)    
     
 class L1Dataterm(Dataterm):
-    def __init__(self, config):
-        super(L1Dataterm, self).__init__(config)
+    def __init__(self):
+        super(L1Dataterm, self).__init__()
 
-    def energy(self, u, u0, gamma):
-        return gamma*torch.sum(torch.abs(u - u0))
+    def energy(self, u, u0, mu):
+        return mu*(u-u0)
+    
+    def prox(self, u, u0, mu, tau):
+        return u0+torch.max(0, torch.abs(u-u0)-tau*mu)*torch.sign(u-u0)
+    
+    def grad(self, u, u0, mu):
+        return mu*torch.sign(u-u0)
+    
 
-    def prox(self, u, u0, gamma, tau):
-        return u0+torch.max(0, torch.abs(u-u0)-tau*gamma)*torch.sign(u-u0)
+class CollaborativeDataFidelity(Dataterm):
+    r"""
+    This dataterm uses the definition defined in paper formula(3).
+    It uses the approximation that candidates are independent from each
+    other.
 
-    def grad(self, u, u0, gamma):
-        return gamma*torch.sign(u-u0) 
+    D(u;\theta)=\frac{lambda}{2}|u^{rgb}-f^0|^2 + 
+                \mu|u^{c}-c|_1 + 
+                \nu|u^d-\hat{d}|)_{u^c, 1}
+    """
+
+class VNet(nn.Module):
+    """
+    Variational Network
+    """
+    def __init__(self):
+        pass
+    
